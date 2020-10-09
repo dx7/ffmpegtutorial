2b0586970e97df37d1d678456f92fde6fa4802f0
dx7 <dx7@protonmail.ch>
2020-10-08 21:34:26 -0300

    [tutorial03] use av_packet_clone and remove deprecated av_dup_packet

--- src/tutorial03.c
+++ src/tutorial03.c
@@ -64,13 +64,18 @@ void packet_queue_init(PacketQueue *q) {
 int packet_queue_put(PacketQueue *q, AVPacket *pkt) {
 
     AVPacketList *pkt1;
-    if(av_dup_packet(pkt) < 0) {
+    AVPacket *p;
+
+    p = av_packet_clone(pkt);
+    if (p == NULL) {
         return -1;
     }
+
     pkt1 = av_malloc(sizeof(AVPacketList));
     if (!pkt1)
         return -1;
-    pkt1->pkt = *pkt;
+
+    pkt1->pkt = *p;
     pkt1->next = NULL;
 
 
@@ -80,12 +85,13 @@ int packet_queue_put(PacketQueue *q, AVPacket *pkt) {
         q->first_pkt = pkt1;
     else
         q->last_pkt->next = pkt1;
+
     q->last_pkt = pkt1;
     q->nb_packets++;
     q->size += pkt1->pkt.size;
     SDL_CondSignal(q->cond);
-
     SDL_UnlockMutex(q->mutex);
+
     return 0;
 }
 static int packet_queue_get(PacketQueue *q, AVPacket *pkt, int block)
